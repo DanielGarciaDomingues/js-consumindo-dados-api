4091fb5e8b263f6c7272c2441260e51a48d48364
Daniel <danielgd.bh@gmail.com>
2023-03-21 19:20:44 -0300

    Projeto concluido: Acessando API viacep atraves de uma fetch API, auto-complete nos campos fornecidos pela API, tratamento de erros.

--- script.js
+++ script.js
@@ -1,27 +1,68 @@
-async function buscaEndereco(cep) {
-    var mensagemErro = document.getElementById('erro');
-    mensagemErro.innerHTML = "";
+    // Notas:
+    // escreve cep
+    // evento focusout 
+    // ativa função assincrona buscaCep 
+    // cep é valido? 
+    // tipos de erro
+    // fetch no banco de dados na variavel await consultaCep
+    // promise gerada
+    //then
+    //catch
+    // convertendo arquivo em .jason()
+    // indexar informações pareadas
+    //Possibilidade 1:
+    //Com then e catch
+
+// function buscaCep1 (cep){
+//     var consultaCEP = fetch(`https://viacep.com.br/ws/${cep}/json`)
+//     .then (resposta =>  console.log(resposta.json()))
+//     .catch (erro => console.log(erro))
+//     .finally(mensagem => console.log('Processamento concluido'))
+
+// }
+
+
+
+async function buscaCep (cep){
+    let mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ""
     try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        var consultaCEPConvertida = await consultaCEP.json();
-        if (consultaCEPConvertida.erro) {
-            throw Error('CEP não existente!');
-        }
-        var cidade = document.getElementById('cidade');
-        var logradouro = document.getElementById('endereco');
-        var estado = document.getElementById('estado');
-
-        cidade.value = consultaCEPConvertida.localidade;
-        logradouro.value = consultaCEPConvertida.logradouro;
-        estado.value = consultaCEPConvertida.uf;
-
-        console.log(consultaCEPConvertida);
-        return consultaCEPConvertida;
-    } catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
-        console.log(erro);
+    let consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json`)
+    const consultaCEPConvertida = await consultaCep.json()
+    if (consultaCEPConvertida.erro){
+        throw Error('Cep não existe!')
     }
+    console.log(consultaCEPConvertida)
+    montaEndereco(consultaCEPConvertida)
+    return consultaCEPConvertida
+} catch (erro) {
+    mensagemErro.innerHTML = `<p>Cep inválido, tente novamente.</p>`
+    console.log(erro)
+} finally {
+    console.log('Processo concluído.')
 }
+}
+//Exemplo Promise.all() para multiplas requisições 
+// let ceps = ['01001-000','01001-001']
+// let consultaCeps = ceps.map(el => buscaCep(el))
+// Promise.all(consultaCeps).then(resposta => console.log(resposta))
 
-var cep = document.getElementById('cep');
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+const cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaCep(cep.value))
+
+
+
+function montaEndereco (consultaCEPConvertida){
+    console.log (consultaCEPConvertida)
+    // Object { cep: "01001-000", logradouro: "Praça da Sé", complemento: "lado ímpar", bairro: "Sé", localidade: "São Paulo", uf: "SP", ibge: "3550308", gia: "1004", ddd: "11", siafi: "7107" }
+    const endereco = document.getElementById('endereco')
+    const complemento = document.getElementById('complemento')
+    const bairro = document.getElementById('bairro')
+    const cidade = document.getElementById('cidade')
+    const estado = document.getElementById('estado')
+        endereco.value = consultaCEPConvertida.logradouro
+        complemento.value = consultaCEPConvertida.complemento
+        bairro.value = consultaCEPConvertida.bairro
+        estado.value = consultaCEPConvertida.uf
+        cidade.value = consultaCEPConvertida.localidade
+}
